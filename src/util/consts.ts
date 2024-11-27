cacab4be2a6249c7baacc507aa99e47f96fed790
David Kerber <dave@kerber.io>
2024-11-26 20:44:38 -0600

    Adding a Sanity Check to the Download and Publishing Process (#1)
    * Adding a sanity check to the update process before any packages are changed.
    
    * Removing test error code from sanity check.

--- src/util/consts.ts
+++ src/util/consts.ts
@@ -11,3 +11,8 @@ export const operatorDetailsLocation = join(operatorsLocation, 'details')
 export const htmlDownloadLocation = join(filesLocation, 'html')
 
 export const jsonDocsLocaiton = join(filesLocation, 'json')
+
+export const serviceInfoLocation = join(filesLocation, 'serviceInfo')
+export const actionsLocation = join(serviceInfoLocation, 'actions')
+export const resourceTypesLocation = join(serviceInfoLocation, 'resourceTypes')
+export const conditionKeysLocation = join(serviceInfoLocation, 'conditionKeys')
